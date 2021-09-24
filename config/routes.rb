7e61d5dc7d9c0ca4c6ec857cdd3413d2e75dbcaa
EC2 Default User <ec2-user@ip-172-31-15-26.ec2.internal>
2021-09-24 01:58:04 +0000

    Fixed Hint4-added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,4 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
 end
 #ヒント1 end追加
-#ヒント3 resources users devise 入れ替え
+#ヒント3 resources users devise 入れ替え エラーがresources users#showを先に書くとuser/:idが先にマッチる=>user/sign_inにアクセスしてしまう
